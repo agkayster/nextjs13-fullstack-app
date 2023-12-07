50f9bde47f1159274e348fca5b72248706a4593b
agkayster <ejike.chiboka@gmail.com>
2023-12-07 13:38:58 +0100

    Added subfolders to blog, portfolio and dashboard

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlogPost = () => {
+	return <div>Welcome to a Blog Post</div>;
+};
+
+export default BlogPost;
---
+++ src/app/portfolio/[category]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CategoryPage = () => {
+	return <div>This is a Category Page</div>;
+};
+
+export default CategoryPage;
