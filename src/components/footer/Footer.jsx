53eb037245732e49733d4dcdcddd647a1a9cb342
agkayster <ejike.chiboka@gmail.com>
2023-12-08 11:15:04 +0100

    Added images to Footer component and refactored code

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,23 @@
+import Image from 'next/image';
 import React from 'react';
+import { images } from './images';
 
 const Footer = () => {
 	return (
-		<div className='h-14 bg-slate-600 flex items-center justify-between'>
-			<div>©️ejikechiboka. All rights reserved</div>
-			<div></div>
+		<div className='h-14 flex items-center justify-between'>
+			<div className='text-sm'>©️ejikechiboka. All rights reserved</div>
+			<div className='flex flex-row gap-2.5'>
+				{images.map(({ id, title }) => (
+					<Image
+						key={id}
+						src={title}
+						width={15}
+						height={15}
+						alt='ejike dev'
+						className='opacity-60 cursor-pointer'
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
---
+++ src/components/footer/images.jsx
@@ -0,0 +1,11 @@
+import numberOnePic from '../../../public/1.png';
+import numberTwoPic from '../../../public/2.png';
+import numberThreePic from '../../../public/3.png';
+import numberFourPic from '../../../public/4.png';
+
+export const images = [
+	{ id: 1, title: numberOnePic },
+	{ id: 2, title: numberTwoPic },
+	{ id: 3, title: numberThreePic },
+	{ id: 4, title: numberFourPic },
+];
