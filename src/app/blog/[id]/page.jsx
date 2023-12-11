4cbad89818ffcc67c92c46fa34b19f55e0d48999
agkayster <ejike.chiboka@gmail.com>
2023-12-11 12:49:27 +0100

    Completed data fetching on server side

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
 import blogAvatar from '../../../../public/blog_avatar.jpeg';
 import mountainImg from '../../../../public/mountain_blog.jpeg';
 
-const BlogPost = ({ params }) => {
-	// console.log('get params =>', params);
+/* we are fetching Data. always use async/await */
+async function getData(id) {
+	const res = await fetch(
+		// id here is coming from params.id passed below
+		`https://jsonplaceholder.typicode.com/posts/${id}`,
+		{
+			cache: 'no-store',
+		}
+	);
+
+	if (!res.ok) {
+		// This will activate the closest `error.js` Error Boundary
+		return notFound();
+	}
+	return res.json();
+}
+
+/* always use async/await */
+const BlogPost = async ({ params }) => {
+	console.log('get params =>', params);
+	const { id } = params;
+
+	/* id here is passed into the getData function above and into the API URL, this must match [id] in the subfolder 
+	category here is passed into the getData function above and into the API URL, this must match [category] in the subfolder 
+	*/
+	const data = await getData(id);
+
 	return (
 		<div className='cont'>
 			<div className='top flex'>
 				<div className='info flex-1'>
-					<h1 className='title text-[2.5rem] '>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					</h1>
-					<p className='desc text-lg font-light'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Perferendis impedit mollitia ea dolorum. Quos eum
-						corporis asperiores optio vel, iure ullam, quo sequi
-						placeat ipsum quas? Itaque deleniti beatae magni.
-					</p>
+					<h1 className='title text-[2.5rem] '>{data.title}</h1>
+					<p className='desc text-lg font-light'>{data.body}</p>
 					<div className='author flex items-center gap-2.5'>
 						<Image
 							src={blogAvatar}
@@ -40,12 +59,7 @@ const BlogPost = ({ params }) => {
 				</div>
 			</div>
 			<div className='content mt-[3.125rem] text-xl font-light text-[#999] text-justify'>
-				<p className='text'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Quasi, accusamus non autem, reiciendis voluptatum pariatur
-					laudantium facere modi cum nisi obcaecati expedita cumque ea
-					laborum, voluptas ipsa dolorum? Pariatur, nesciunt!
-				</p>
+				<p className='text'>{data.body}</p>
 			</div>
 		</div>
 	);
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,43 +4,48 @@ import Image from 'next/image';
 
 import blogImg from '../../../public/blog_image.jpeg';
 
-const BlogPage = () => {
+/* we are fetching Data */
+async function getData() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+		next: { revalidate: 10 }, // => revalidates our data every 10 secs
+	});
+
+	if (!res.ok) {
+		// This will activate the closest `error.js` Error Boundary
+		throw new Error('Failed to fetch data');
+	}
+	return res.json();
+}
+
+const BlogPage = async () => {
+	const data = await getData();
+
 	return (
 		<div className='mainCont'>
-			<Link
-				href='/blog/testId1'
-				className='cont flex items-center gap-[3.125rem] mb-[3.125rem]'>
-				<div className='imgContainer'>
-					<Image
-						alt='blog image'
-						src={blogImg}
-						width={400}
-						height={150}
-						className='img object-cover'
-					/>
-				</div>
-				<div className='content'>
-					<h1 className='title mb-2.5'>Test</h1>
-					<p className='desc text-lg text-[#999]'>Desc</p>
-				</div>
-			</Link>
-			<Link
-				href='/blog/testId2'
-				className='cont flex items-center gap-[3.125rem] mb-[3.125rem]'>
-				<div className='imgContainer'>
-					<Image
-						alt='blog image'
-						src={blogImg}
-						width={400}
-						height={150}
-						className='img object-cover'
-					/>
-				</div>
-				<div className='content'>
-					<h1 className='title mb-2.5'>Test</h1>
-					<p className='desc text-lg text-[#999]'>Desc</p>
-				</div>
-			</Link>
+			{data.map(({ userId, id, title, body }) => (
+				<Link
+					key={userId}
+					/* id in href must match [id] subfolder or category in href url must match [category] subfolder
+					id or category or house property must also be present as a property in the API json data.
+					There must be a property called id, house, category in the API json data
+					*/
+					href={`/blog/${id}`}
+					className='cont flex items-center gap-[3.125rem] mb-[3.125rem]'>
+					<div className='imgContainer'>
+						<Image
+							alt='blog image'
+							src={blogImg}
+							width={400}
+							height={150}
+							className='img object-cover flex-1'
+						/>
+					</div>
+					<div className='content flex-1'>
+						<h1 className='title mb-2.5'>{title}</h1>
+						<p className='desc text-lg text-[#999]'>{body}</p>
+					</div>
+				</Link>
+			))}
 		</div>
 	);
 };
