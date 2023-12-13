1b32e995e19ad1d737541c10a67382974a66c2ee
agkayster <ejike.chiboka@gmail.com>
2023-12-13 12:34:12 +0100

    Applying and consuming session via hooks

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -20,16 +20,16 @@ export const GET = async (request, { params }) => {
 	}
 };
 
-export const DELETE = async (request, { params }) => {
-	const { id } = params;
+// export const DELETE = async (request, { params }) => {
+// 	const { id } = params;
 
-	try {
-		await connect();
+// 	try {
+// 		await connect();
 
-		await Post.findByIdAndDelete(id);
+// 		await Post.findByIdAndDelete(id);
 
-		return new NextResponse('Post has been deleted', { status: 200 });
-	} catch (err) {
-		return new NextResponse('Database Error', { status: 500 });
-	}
-};
+// 		return new NextResponse('Post has been deleted', { status: 200 });
+// 	} catch (err) {
+// 		return new NextResponse('Database Error', { status: 500 });
+// 	}
+// };
